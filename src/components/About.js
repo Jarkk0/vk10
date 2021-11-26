8035f863e67c0c61d8de4f11d1dddb715e4b510d
Jarkk0 <uusijarkko@gmail.com>
2021-11-26 13:09:19 +0200

    about haku

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react"
 
 function About() {
-    return (
-        <div>
-            <h2>About teksti</h2>
-        </div>
-    )
+    let [listItems, setSomeStuff] = useState([]);
+
+    useEffect(() => {
+        let mounted = true;
+        const doStuff = async () => {
+            const a = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const b = await a.json(); //pitää tehdä jotain, että saadaan oikea data a:sta
+            if(mounted) {
+                setSomeStuff(b);
+            }
+            //tarkastetaan, että komponentti on tarkistettu
+            
+        };
+        doStuff();
+        return () => {
+            mounted = false;
+        };
+
+        //Jos komponenttia ei ole irroitettu, useEffect pysähtyy ja palautuu
+        //Palautuksessa asetetaan vääräksi ja tehdään doStuff funktio
+    
+
+
+    }, []);
+
+    const postList = (
+        <ul>
+            {listItems.map((b) =>(
+                <li key={b.id}>{b.title}</li>
+            ))}
+        </ul>
+    );
+
+    if (listItems) {
+        return <div>{listItems && postList}</div>;
+    }
+    else {
+        return null;
+    }
+
 }
 
-export default About
+export default About;
