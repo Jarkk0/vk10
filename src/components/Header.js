9f1409e0b6cb3a807afb5d19b776df2eabfc6d92
Jarkk0 <uusijarkko@gmail.com>
2021-11-26 15:40:47 +0200

    fallback

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,18 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { Link as RouterLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { Suspense } from 'react';
+
 
 export default function Header() {
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (lng) => {
+      i18n.changeLanguage(lng);
+    }
+
     return (
+      <Suspense fallback= "perkele">
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
             <Toolbar>
@@ -20,10 +29,14 @@ export default function Header() {
               
               ></IconButton>
                 
-              <Button component={RouterLink} to="/" color="inherit">Home</Button>
-              <Button component={RouterLink} to="/about" color="inherit">About</Button>
+              <Button component={RouterLink} to="/" color="inherit">{t('Home')}</Button>
+              <Button component={RouterLink} to="/about" color="inherit">{t('About')}</Button>
+              <Button onClick={() => changeLanguage('en')} color="inherit">EN</Button>
+              <Button onClick={() => changeLanguage('fi')} color="inherit">FI</Button>
             </Toolbar>
           </AppBar>
         </Box>
+      </Suspense>  
       );
 }
+
--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,11 +1,20 @@
-
+import { useTranslation } from 'react-i18next'
+import { Suspense } from 'react';
 
 function MyContainer() {
+    const { t, i18n } = useTranslation();
     return (
+        
         <div>
-            <h3>MyContainer sisältö</h3>
+            <h3>{t('This is the front page')}</h3>
         </div>
     )
 }
 
-export default MyContainer
+export default function App() {
+    return(
+        <Suspense fallback="perkele">
+            <MyContainer />
+        </Suspense>
+    )
+}
